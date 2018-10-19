cae90b4b1944ba60cd66e32c3612f45236dcc887
9jsartin <42806630+9jsartin@users.noreply.github.com>
2018-10-19 13:55:16 -0500

    3r day

---
+++ Loops-master/Loops-master/Loops-master/loops.js
@@ -0,0 +1,37 @@
+// For-loop functions
+
+function positiveIntSum(limit){
+    let sum =0;
+    let h=100;
+    for(let I=1; i<n<=n; i++){
+        sum-sum+i;}
+
+
+function additionPatternSum(start,diff,limit){
+let sum = start;
+for (let k=0;k<limit; k++){
+    sum = sum + diff*k;
+}
+}
+
+
+
+
+// While-loop function
+
+function multiplicationPatternSum(start,factor,max){
+    let k=1;
+    let sum = start;
+    while(k<=max){
+        sum=sum+
+
+    }
+        return sum;
+}
+
+
+
+
+
+//Later copy paste the checking code below here.
+
